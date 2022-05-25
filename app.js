6da64b5cf851d2e71da3f47316abfe7a26dbc011
Vidushi-CU <vidushi0458.cse19@chitkara.edu.in>
2022-05-25 19:19:07 +0530

    Done with the backend

---
+++ app.js
@@ -0,0 +1,24 @@
+// console.log("Book Store opened!!");
+
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require("./routes/book-routes");
+
+const app = express();
+
+//Middlewares
+app.use(express.json());
+
+// app.use('/',(req,res,next)=>{
+//     res.send("This is our starting app");
+// })
+
+app.use("/books",router) //localhost:5000/books
+
+
+mongoose.connect("mongodb+srv://admin:12fvCakc1Hwv58Ew@cluster0.gvcit.mongodb.net/BookStore?retryWrites=true&w=majority"
+).then(()=> console.log("COnnected to database"))
+.then(()=>{
+    app.listen(5000)
+})
+.catch((err)=> console.log(err));
