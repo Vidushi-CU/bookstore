123c165ab09149c90da81fb6eb4522221898ebfe
Vidushi-CU <vidushi0458.cse19@chitkara.edu.in>
2022-05-26 19:33:23 +0530

    Done with the Add Product

--- book-store/src/Components/AddBook.js
+++ book-store/src/Components/AddBook.js
@@ -1,9 +1,49 @@
 import React from 'react'
-import {FormLabel, TextField,Box, Button} from '@mui/material';
+import { useState } from 'react';
+import {FormControlLabel, Checkbox, FormLabel, TextField,Box, Button} from '@mui/material';
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
 
 const AddBook = () => {
+    const history= useNavigate();
+    const [inputs, setInputs] = useState({
+        name:'',
+        description:'',
+        price:'',
+        author:'',
+        image:''
+    });
+
+    const [checked, setChecked] = useState(false)
+
+    const handleChange = (e)=>{
+        setInputs((prevState)=>({
+            ...prevState,
+            [e.target.name]:e.target.value
+        }))
+    };
+
+    const sendRequest = async() =>{
+        await axios.post("http://localhost:5000/books",{
+            name:String(inputs.name),
+            author: String(inputs.author),
+            description:String(inputs.description),
+            price:Number(inputs.price),
+            image:String(inputs.image),
+            availability: Boolean(checked)
+        }).then(res => res.data);
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(inputs,checked);
+        sendRequest().then(()=>history('/books'));
+    }
+
+
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
         <Box display="flex" flexDirection="column" justifyContent={'center'} maxWidth={700}
         alignContent={"center"}
         alignSelf="center"
@@ -12,16 +52,21 @@ const AddBook = () => {
         paddingTop={10}
         >
         <FormLabel>Name</FormLabel>
-        <TextField margin="normal" fullWidth variant="outlined" name="name" />
+        <TextField value={inputs.name} onChange={handleChange} margin="normal" fullWidth variant="outlined" name="name" />
 
         <FormLabel>Author</FormLabel>
-        <TextField margin="normal" fullWidth variant="outlined" name="name" />
+        <TextField value={inputs.author} onChange={handleChange} margin="normal" fullWidth variant="outlined" name="author" />
 
         <FormLabel>Description</FormLabel>
-        <TextField margin="normal" fullWidth variant="outlined" name="name" />
+        <TextField value={inputs.description} onChange={handleChange} margin="normal" fullWidth variant="outlined" name="description" />
 
         <FormLabel>Price</FormLabel>
-        <TextField type={"number"} margin="normal" fullWidth variant="outlined" name="name" />
+        <TextField value={inputs.price} onChange={handleChange} type={"number"} margin="normal" fullWidth variant="outlined" name="price" />
+
+        <FormLabel>Image url</FormLabel>
+        <TextField value={inputs.image } onChange={handleChange} margin="normal" fullWidth variant="outlined" name="image" />
+
+        <FormControlLabel control={<Checkbox checked={checked} onChange={()=>setChecked(!checked)} />} label="Available in the Stock" />
 
         <Button variant="contained" type="submit">Add Product</Button>
 
